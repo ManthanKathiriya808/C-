dc58dc35ae00a02be6fad37aaa11bb2bc00ea8db
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2024-10-03 17:47:57 +0530

    esdfdsgf

---
+++ 1st lecture/index.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a= 20;
+    int b =10;
+
+    cout << "addition "  << a+b << endl;
+    cout << "sub "  << a-b << endl;
+    cout << "mult "  << a*b << endl;
+    cout << "div "  << a/b << endl;
+    cout << "modulo "  <<a%b << endl;
+
+}
+
+
+
