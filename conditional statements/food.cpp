e2f9dd20f81e26a51f68835600e6d87948699306
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2024-10-09 15:48:23 +0530

    sdrgfdgsfdg

--- conditional statements/food.cpp
+++ conditional statements/food.cpp
@@ -10,9 +10,9 @@ int main (){
 
     cout<< "menu"<< endl;
     cout << "enter 1 for pizza" << endl;
-    cout << "enter 2 for pizza" << endl;
-    cout << "enter 3 for pizza" << endl;
-    cout << "enter 4 for pizza" << endl;
+    cout << "enter 2 for burder" << endl;
+    cout << "enter 3 for sub" << endl;
+    cout << "enter 4 for pasta" << endl;
 
     cin >> a;
 
@@ -24,19 +24,21 @@ int main (){
     cout << "enter 3 for 8 cheeze" << endl;
     cout << "enter 4 for spicy garlic " << endl;
 
+    cin >> b;
+
     if(b==1){
             cout << "enter 1 for margirita" << endl;
     }
 
-    if(b==2){
+    else if(b==2){
               cout << "enter 2 for tandoori" << endl;
     }
 
-    if(b==3){
+    else if(b==3){
           cout << "enter 3 for 8 cheeze" << endl;
     }
 
-    if(b==4){
+    else if(b==4){
               cout << "enter 4 for spicy garlic " << endl;
 
     }
@@ -44,18 +46,93 @@ int main (){
     }
 
 
-    if(a==2){
+    else if(a==2){
         cout << "burgger";
+          cout<< "burder type"<< endl;
+    cout << "enter 1 for sezwan" << endl;
+    cout << "enter 2 for double tikke" << endl;
+    cout << "enter 3 for Aloo Tikke" << endl;
+    cout << "enter 4 for spicy mexican " << endl;
+
+cin >> b;
+
+    if(b==1){
+            cout << "enter 1 for sezwan" << endl;
     }
 
-    if(a==3){
+   else  if(b==2){
+              cout << "enter 2 for double tikee" << endl;
+    }
+
+   else  if(b==3){
+          cout << "enter 3 for Aloo tikke" << endl;
+    }
+
+    else if(b==4){
+              cout << "enter 4 for spicy mexican " << endl;
+
+    }
+    }
+
+   else if(a==3){
         cout << "pasta";
+          cout<< "pasta type"<< endl;
+    cout << "enter 1 for red sauce" << endl;
+    cout << "enter 2 for white sauce" << endl;
+    cout << "enter 3 for italian style" << endl;
+    cout << "enter 4 for spicy tricolor pasta " << endl;
+
+    cin >> b;
+
+    if(b==1){
+            cout << "enter 1 for red sauce" << endl;
     }
 
-    if(a==4){
+    else if(b==2){
+              cout << "enter 2 for white sauce" << endl;
+    }
+
+   else  if(b==3){
+          cout << "enter 3 for italian" << endl;
+    }
+
+    else if(b==4){
+              cout << "enter 4 for tricolor pasta " << endl;
+
+    }
+    }
+
+   else if(a==4){
         cout << "sub";
+          cout<< "sub type"<< endl;
+    cout << "enter 1 for garlic bread" << endl;
+    cout << "enter 2 for wheat breed" << endl;
+    cout << "enter 3 for multi grain" << endl;
+    cout << "enter 4 for corn breaad " << endl;
+
+    cin >> b;
+
+    if(b==1){
+            cout << "enter 1 for garlic" << endl;
+            
     }
 
+   else  if(b==2){
+              cout << "enter 2 for wheat bread" << endl;
+    }
 
+    else if(b==3){
+          cout << "enter 3 for multi brad" << endl;
+    }
+
+    else if(b==4){
+              cout << "enter 4 for corn bread " << endl;
+
+    }
+    }
+
+else{
+    cout << "no orsder found";
+}
   
 }
