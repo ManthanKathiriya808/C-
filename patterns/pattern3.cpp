dcdc0ae4558b07d2a47a65eb096a097e4daaa4f3
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2024-10-18 16:33:08 +0530

    zxddsf

---
+++ patterns/pattern3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+ int p;
+
+for(int i=1;i<=5;i++){
+    if(i%3==0){
+                for(int j=1 ;j<=10 ;j++){
+                 cout<< "*";
+    }
+        }
+
+        else{
+            cout<<" ";
+        }
+
+
+    cout <<  endl;
+}
+
+}
