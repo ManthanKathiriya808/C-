6d990edc39b632c5b19131c0d836b778483cfb57
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2024-11-01 14:18:45 +0530

    asdgf

--- home/tp.cpp
+++ home/tp.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
+
 using namespace std;
 
-
 int main(){
-int age;
-cout << "enter your age";
 
-cin >> age;
+   int i=2;
+   bool prime =true;
+    int n=10;
+   while (i<=n-1){
+        if(n%i==0){
+           prime=false;
+           break;
+        }
 
-cout << "your age is" << age;
-    return 0;
+        i++;
+   }
+
 }
